5ab6c6d36593905ca743b956eb0b662ff8eff7c4
Brett Robinson <octo.banana93@gmail.com>
2018-02-20 04:03:50 -0700

    update examples

--- examples/basic/src/main_m8.cc
+++ examples/basic/src/main_m8.cc
@@ -1,21 +1,30 @@
+#[M8[license ("MIT", "your_name_here", "2018")]]
+
 #include <string>
 #include <iostream>
 
-#[M8[add(int)]]
+#[M8[add int]]
 
-#[M8[add(std::string)]]
+#[M8[add std::string]]
 
-#[M8[oops(64)]]
+#[M8[oops 64]]
 
-int main(int argc, char *argv[])
-{
-  std::string msg {#[M8[repeat(hello,2)]]};
+#[M8[main (
 
   auto cat1 = add(5, 6);
-  auto cat2 = add(msg, "hi");
+  auto cat2 = add("hello", " world");
 
-  std::cout << "cat1: " << cat1 << "\n";
-  std::cout << "cat2: " << cat2 << "\n";
+  std::string repeated {"#[M8[repeat (- EOF, 16)]]"};
+
+  std::string env_home {"#[M8[env HOME]]"};
+
+  std::string sh_example {R"STR(#[M8[sh (printf "Hello, M8!" DONE)]])STR"};
+
+  std::string file_contents {R"STR(#[M8[file ~/.m8.json]])STR"};
+
+  std::string my_ip {R"(#[M8[sh (printf "%s" $(curl -s http://ip.jsontest.com/) DONE)]])"};
+
+  std::string external_macro {R"(#[M8[printf "hello %s" "world" EOF]])"};
 
   return 0;
-}
+)EOF]]
